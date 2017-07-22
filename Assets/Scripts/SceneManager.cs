9f84126e2a2d8415645e1483a2bf7a15f1ab808e
unknown <jeffrimko@gmail.com>
2017-07-22 10:42:06 -0400

    Added logic to quit demo.

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -18,6 +18,9 @@ public class SceneManager : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
+        if(Input.GetKey("escape")) {
+            Application.Quit();
+        }
         sceneSpeed = (player.Speed() / MAX_SPEED_SCALE) + MIN_SCENE_SPEED;
         Time.timeScale = sceneSpeed;
         Time.maximumDeltaTime = sceneSpeed;
